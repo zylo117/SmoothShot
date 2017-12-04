4eaf8e64b4658a93168836ae568775ca4e349c40
zylo117 <zylo117@hotmail.com>
2017-12-04 20:29:08 +0800

    Done

--- frame_insert.py
+++ frame_insert.py
@@ -3,6 +3,7 @@ from imutils.video.videostream import VideoStream
 import cv2
 from imutils.video import FPS
 import datetime
+import numpy as np
 
 # if a video path was not supplied, grab the reference to the webcam
 camera = VideoStream(src=0).start()
@@ -11,6 +12,7 @@ fps = FPS().start()
 current_fps = 0
 
 # keep looping
+old_centroid = [0, 0]
 while True:
     frame = camera.read()
 
@@ -24,8 +26,26 @@ while True:
     if fps._numFrames % 2 == 0:
         insert_frame = frame
 
-    if fps._numFrames % 2 == 1:
-        cv2.merge([insert_frame, frame])
+    # 计算帧间重心Delta矢量
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (11, 11), 0)
+    cv2.threshold(blurred, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY_INV, dst=blurred)
+    M = cv2.moments(blurred)  # 计算矩
+    cx = int(M['m10'] / M['m00'])  # 计算重心
+    cy = int(M['m01'] / M['m00'])  # 计算重心
+    new_centroid = [cx, cy]
+    # cv2.circle(frame, (new_centroid[0], new_centroid[1]), 5, (255,0,255), 5)
+    # dist = np.sqrt((old_centroid[0]-new_centroid[0])**2 + (old_centroid[1]-new_centroid[1])**2)
+    if (old_centroid[0]-new_centroid[0]) != 0:
+        theta = (old_centroid[1]-new_centroid[1]) / (old_centroid[0]-new_centroid[0])
+    else:
+        theta = 0
+    delta_vector = (old_centroid[0]-new_centroid[0], old_centroid[1]-new_centroid[1], theta)
+    # insert_frame = imutils.translate(frame, old_centroid[0]-new_centroid[0], old_centroid[1]-new_centroid[1])
+    insert_frame = imutils.rotate(frame, np.arctan(theta / 4),(new_centroid[0], new_centroid[1]))
+    # cv2.imshow("insert", blurred)
+
+    frame = frame[50:300, 50:350]
 
     if fps._numFrames % 2 == 1:
         # 帧率计数
@@ -37,9 +57,11 @@ while True:
     # 出图
     cv2.imshow("Frame", frame)
 
+    old_centroid = [cx, cy]
+
     # 播放插入帧
     if fps._numFrames % 2 == 1:
-        cv2.imshow("Frame", insert_frame)
+        cv2.imshow("Frame", insert_frame[50:350, 50:350])
 
     key = cv2.waitKey(1) & 0xFF
 
--- insertion_generate.py
+++
@@ -1,2 +0,0 @@
-def make_frame(image)：
-
